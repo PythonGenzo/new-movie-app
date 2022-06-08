31971ea0f1e81e93c29a2ed104c07af2de121ce5
PythonGenzo <sanprasanth94@gmail.com>
2022-06-08 15:56:58 +0530

    API changed and heroku connect

--- src/global.js
+++ src/global.js
@@ -1,1 +1,6 @@
-export const API = 'https://624e6f9a53326d0cfe5b222d.mockapi.io'
+// export const API = 'https://624e6f9a53326d0cfe5b222d.mockapi.io'
+
+// export const API = 'http://localhost:4000'
+
+
+export const API = 'https://genzo-server.herokuapp.com'
